46d06316dd41445a0b50fdaeadecef24615ab67d
Kody Bishop <55988596+kodypbishop@users.noreply.github.com>
2019-11-12 12:18:37 -0700

    using html form to fill out javascript variables

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+var upperLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var unicode = ['\u0020', '\u0021', '\u0022', '\u0023', '\u0024', '\u0025', '\u0026', '\u0027', '\u0028', '\u0029', '\u002A', '\u002B', '\u002C', '\u002D', '\u002E', '\u002F', '\u003A', '\u003B', '\u003C', '\u003D', '\u003E', '\u003F', '\u0040', '\u005B', '\u005C', '\u005D', '\u005E', '\u005F', '\u0060', '\u007B', '\u007C', '\u007D', '\u007E']
+var intergers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var slider = document.getElementById("passwordLength");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
+function random(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+function generatePassword() {
+    var password = ""
+    var includedInPassord = []
+    var upper = document.getElementById("upperLetters").checked;
+    var lower = document.getElementById("lowerLetters").checked;
+    var numbers = document.getElementById("numbers").checked;
+    var symbols = document.getElementById("symbols").checked;
+
+    if (upper == true) { includedInPassord.push(upperLetter); }
+    if (lower == true) { includedInPassord.push(lowerLetter); }
+    if (symbols == true) { includedInPassord.push(unicode); }
+    if (numbers == true) { includedInPassord.push(intergers); }
+
+    for (let i = 0; i < slider.value; i++) {
+        password += random(random(includedInPassord))
+    }
+    return document.getElementById("result").innerText = password;
+}
+function copy_password() {
+    var copyText = document.getElementById("result");
+    var textArea = document.createElement("textarea");
+    textArea.value = copyText.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("Copy");
+}
