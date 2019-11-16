b1b4dc14ae5eac70f39f05def08e7e871b3551f8
Kody Bishop <kodypbishop@gmail.com>
2019-11-15 17:52:13 -0700

    added a validation function to ensure at least one of every selected character appears in the password

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,33 +4,60 @@ var unicode = ['\u0020', '\u0021', '\u0022', '\u0023', '\u0024', '\u0025', '\u00
 var intergers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 var slider = document.getElementById("passwordLength");
 var output = document.getElementById("demo");
+var bool = false
 output.innerHTML = slider.value;
 
 slider.oninput = function () {
     output.innerHTML = this.value;
 }
+// a function to randomly select an element of an array
 function random(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
+// a function to make sure that the generated password contains at least one of each selected charcter type 
+function validate(arr, pass) {
+
+    for (let i = 0; i < arr.length; i++) {
+        var checkArray = 0
+        for (let j = 0; j < pass.length; j++) {
+            if (arr[i].indexOf(pass.charAt(j)) > -1) {
+                checkArray++;
+            }
+        }
+        if (checkArray == 0) {
+            bool = false;
+            console.log("password fail");
+            console.log(pass);
+        }else{
+        }
+    }
+}
+// the password generating function connected to html through onclick in the html button tag
 function generatePassword() {
-    var password = "";
-    var includedInPassord = [];
     var upper = document.getElementById("upperLetters").checked;
     var lower = document.getElementById("lowerLetters").checked;
     var numbers = document.getElementById("numbers").checked;
     var symbols = document.getElementById("symbols").checked;
+    bool = false;
+// a while loop that will stop when the password passes the validation test
+    while (!bool) {
+        bool = true
+        var password = "";
+        var includedInPassord = [];
+        if (upper) { includedInPassord.push(upperLetter); }
+        if (lower) { includedInPassord.push(lowerLetter); }
+        if (symbols) { includedInPassord.push(unicode); }
+        if (numbers) { includedInPassord.push(intergers); }
+        if (!upper && !lower && !symbols && !numbers) { alert("password must have something in it") }
 
-    if (upper) { includedInPassord.push(upperLetter); }
-    if (lower) { includedInPassord.push(lowerLetter); }
-    if (symbols) { includedInPassord.push(unicode); }
-    if (numbers) { includedInPassord.push(intergers); }
-    if(!upper && !lower && !symbols && !numbers){alert("password must have something in it")}
- 
-    for (let i = 0; i < slider.value; i++) {
-        password += random(random(includedInPassord))
+        for (let i = 0; i < slider.value; i++) {
+            password += random(random(includedInPassord))
+        }
+        validate(includedInPassord, password);
     }
     return document.getElementById("result").innerText = password;
 }
+// the password copy function connected to html through onclick in the html button tag
 function copy_password() {
     var copyText = document.getElementById("result");
     copyText.select();
